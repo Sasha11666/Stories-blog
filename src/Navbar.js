3d2b97dea33bbbd682f95184b8b9b030894d3c7a
Sasha11666 <basovasasa087@gmail.com>
2023-07-15 16:44:38 +0300

    customized cursor

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Horror stories </h1>
+      <h1>Horror stories 👻</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New story</Link>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
